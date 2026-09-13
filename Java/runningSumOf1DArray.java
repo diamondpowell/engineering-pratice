332c69d976e7f692812b16814397642414f81a2a
engineeredcurlz <32712461+engineeredcurlz@users.noreply.github.com>
2026-09-12 21:33:36 -0400

    code

---
+++ Java/runningSumOf1DArray.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+        main p = new main();
+        System.out.println(p.sum(2, 3)); // Change this test
+    }
+
+    // Paste your CodingBat method here
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
+
+// keep track of running total
+// loop through the numbers
+// add each number to the running total
+// store each running total
+
+int runningTotal = 0;
