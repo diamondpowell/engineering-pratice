b40b52b00e119700e35f4d1c911afb2d789ffc9b
engineeredcurlz <32712461+engineeredcurlz@users.noreply.github.com>
2026-09-15 20:10:28 -0400

    code

---
+++ Java/warmup-1/sleepIn.java
@@ -0,0 +1,21 @@
+public boolean sleepIn(boolean weekday, boolean vacation) 
+{
+  // weekday = is today a weekday?
+  // vacation = are we on vacation?
+  
+  // if !weekday || vacation
+    // return true
+  // if weekday || !vacation
+    // return false
+  //if weekday || vacation
+    // return true
+  
+  if (!weekday || vacation)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
